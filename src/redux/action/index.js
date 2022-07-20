b4e0f2d5e34aeccc624370170e826a2cb8108de1
NidhiNivedita6 <nidhinivedita01@gmail.com>
2022-07-21 02:18:23 +0530

    Commit

---
+++ src/redux/action/index.js
@@ -0,0 +1,15 @@
+//For adding item to cart
+export const addCart = (product) => {
+    return{
+        type: "ADDITEM",
+        payload: product
+    }
+}
+
+// For deleting item from cart
+export const delCart = (product) => {
+    return{
+        type: "DELITEM",
+        payload: product
+    }
+}
